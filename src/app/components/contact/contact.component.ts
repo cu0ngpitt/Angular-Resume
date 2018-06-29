2e594f4b562fe3b21f0a70bac9a81aab65a9ed5a
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-29 13:44:27 -0500

    Added functionality to the contact form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var emailjs: any;
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,7 +11,23 @@ export class ContactComponent implements OnInit {
 
   constructor() { }
 
+  submitted = false;
+
   ngOnInit() {
   }
 
+  onSubmit(contactform) {
+    const service_id = 'mailgun',
+          template_id = 'mailgun',
+          template_params = {
+              name: contactform.value.name,
+              email: contactform.value.email,
+              message: contactform.value.message
+              };
+
+    emailjs.send(service_id, template_id, template_params);
+
+    this.submitted = true;
+    contactform.reset();
+  }
 }
