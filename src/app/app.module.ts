2743c4e32ff728ddae8c86d5418ca68062a8fdf9
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-29 13:40:57 -0500

    Added forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -42,7 +43,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [SkillsService, EducationService, ProjectService],
   bootstrap: [AppComponent]
